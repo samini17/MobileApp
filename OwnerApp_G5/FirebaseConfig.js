690c5367e9b867fbc42fd674aaad8bd10b91b3d7
The Punslinger <59809715+Pun-slinger@users.noreply.github.com>
2024-08-11 17:59:08 -0400

    commit

--- OwnerApp_G5/FirebaseConfig.js
+++ OwnerApp_G5/FirebaseConfig.js
@@ -6,18 +6,7 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 //import any services required from firebase
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"
-
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBCAj1YR8jroIoIzG-pL535Wp-PdrFsgmE",
-//   authDomain: "rentanthing-28dd4.firebaseapp.com",
-//   projectId: "rentanthing-28dd4",
-//   storageBucket: "rentanthing-28dd4.appspot.com",
-//   messagingSenderId: "482782187348",
-//   appId: "1:482782187348:web:063c7aff1a78ce43f742c1"
-// };
 
 const firebaseConfig = {
   apiKey: "AIzaSyC6IileYxtY2BnDeEFt5dkFWuILVQgTmBs",
